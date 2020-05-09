afc1c2e643ff99148fcc539f28c68c4ed1baa944
Russell Gordon <rgordon@lcs.on.ca>
2020-05-09 07:50:18 -0400

    Initial Commit

---
+++ SortRace/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SortRace
+//
+//  Created by Russell Gordon on 2020-05-09.
+//  Copyright © 2020 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
