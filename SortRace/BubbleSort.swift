934aa671e2eb3ecc82d006eb12219dea4f650036
Russell Gordon <rgordon@lcs.on.ca>
2020-05-11 16:29:16 -0400

    Added optimized bubble sort algorithm.

--- SortRace/BubbleSort.swift
+++ SortRace/BubbleSort.swift
@@ -12,9 +12,13 @@ func bubbleSort(dataSet: inout [Int]) {
     
     // Loop through the entire array "n" times
     // (however many times there are elements in the array)
-    for _ in 0..<dataSet.count {
+    var swapOccured = true
+    while swapOccured {
 
-        // One pass through the array to float the highest number to the end
+        // Starting a new pass, so reset swap flag
+        swapOccured = false
+        
+        // Pass through the array to float the highest number to the end
         for j in 0..<dataSet.count - 1 {
             
             // Compare left value to right value
@@ -23,15 +27,18 @@ func bubbleSort(dataSet: inout [Int]) {
                 let temporaryValue = dataSet[j] // Set aside the left value
                 dataSet[j] = dataSet[j + 1]     // Replace left with right
                 dataSet[j + 1] = temporaryValue // Replace right with temporary value
+                swapOccured = true
             }
             
         }
 
-        // DEBUG: Print the array after n passes
+//        // DEBUG: Print the array after n passes
 //        print("Array after pass \(i + 1):")
 //        print(dataSet)
-
+        
     }
 
+//    // DEBUG: Print the sorted array
+//    print("array after sort is: \(dataSet)")
     
 }
