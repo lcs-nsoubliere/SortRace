4d38adfacf8f33e092e7874f2f0ff23806830f51
Russell Gordon <rgordon@lcs.on.ca>
2020-05-11 16:20:03 -0400

    Unoptimized bubble sort and time reporting added.

---
+++ SortRace/BubbleSort.swift
@@ -0,0 +1,37 @@
+//
+//  BubbleSort.swift
+//  SortRace
+//
+//  Created by Russell Gordon on 2020-05-11.
+//  Copyright © 2020 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+func bubbleSort(dataSet: inout [Int]) {
+    
+    // Loop through the entire array "n" times
+    // (however many times there are elements in the array)
+    for _ in 0..<dataSet.count {
+
+        // One pass through the array to float the highest number to the end
+        for j in 0..<dataSet.count - 1 {
+            
+            // Compare left value to right value
+            if dataSet[j] > dataSet[j + 1] {
+                // Swap values when left value is more than right value
+                let temporaryValue = dataSet[j] // Set aside the left value
+                dataSet[j] = dataSet[j + 1]     // Replace left with right
+                dataSet[j + 1] = temporaryValue // Replace right with temporary value
+            }
+            
+        }
+
+        // DEBUG: Print the array after n passes
+//        print("Array after pass \(i + 1):")
+//        print(dataSet)
+
+    }
+
+    
+}
--- SortRace/Race.swift
+++ SortRace/Race.swift
@@ -10,33 +10,69 @@ import Foundation
 
 func race(algorithm: SortAlgorithm, valuesProfile: ArrayCharacteristic, size: ArraySize, trials: TrialCount) {
     
-    // Create an empty array
-    var values: [Int] = []
-    
-    // Populate the array
-    switch valuesProfile {
-    case .almostSorted:
-        for i in 1...Int(size.description)! - 1 {
-            values.append(i * 3)
-        }
-        values.append(1)
-    case .backwards:
-        for i in stride(from: Int(size.description)!, through: 1, by: -1) {
-            values.append(i * 3)
-        }
-    case .random:
-        for _ in 1...Int(size.description)! {
-            values.append(Int.random(in: 1...Int(size.description)!))
-        }
-    case .alreadySorted:
-        for i in 1...Int(size.description)! {
-            values.append(i * 3)
-        }
-    }
-    
 //    // DEBUG
 //    print(values.count)
 //    print(values)
     
+    // Sort the array the specified number of times
+    print("")
+    var totalTime: TimeInterval = 0
+    for trial in 1...Int(trials.description)! {
+
+        // Create an empty array
+        var values: [Int] = []
+        
+        // Populate the array
+        switch valuesProfile {
+        case .almostSorted:
+            for i in 1...Int(size.description)! - 1 {
+                values.append(i * 3)
+            }
+            values.append(1)
+        case .backwards:
+            for i in stride(from: Int(size.description)!, through: 1, by: -1) {
+                values.append(i * 3)
+            }
+        case .random:
+            for _ in 1...Int(size.description)! {
+                values.append(Int.random(in: 1...Int(size.description)!))
+            }
+        case .alreadySorted:
+            for i in 1...Int(size.description)! {
+                values.append(i * 3)
+            }
+        }
+
+        // Report trial start
+        print("Trial \(trial) starting...", terminator: "")
+        
+        // Time in seconds since the epoch
+        let startTime = Date().timeIntervalSince1970
+        
+        // CALL THE NECESSARY SORT FUNCTION
+        switch algorithm {
+        case .bubbleSort:
+            bubbleSort(dataSet: &values)
+        default:
+            break
+        }
+
+        // Time in seconds since the epoch
+        let endTime = Date().timeIntervalSince1970
+        
+        // Get elapsed time
+        let elapsedTime = endTime - startTime
+        
+        // Report time for this sort
+        print("ended. Elapsed time in seconds was: \(elapsedTime)")
+        
+        // Add to total time
+        totalTime += elapsedTime
+
+    }
+    
+    // Report the average time per trial
+    let averageTime = totalTime / TimeInterval(trials.description)!
+    print("\nAverage time per trial, with \(Int(trials.description)!) trials, was: \(averageTime)\n")
     
 }
