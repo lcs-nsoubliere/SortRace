c2cadf3b98fc1aef4dd810106dffb29c180d7391
Russell Gordon <rgordon@lcs.on.ca>
2020-05-12 07:30:48 -0400

    Improve comments and refactor bubble sort code for readability

--- SortRace/BubbleSort.swift
+++ SortRace/BubbleSort.swift
@@ -10,39 +10,45 @@ import Foundation
 
 func bubbleSort(dataSet: inout [Int]) {
     
-    // Loop through the entire array "n" times
-    // (however many times there are elements in the array)
+    // Tracks whether a swap occured on the prior pass through the array
     var swapOccured = true
-    var sortedValues = 0    // Tracks how many values have been floated to the end of the array
+    
+    // Tracks how many values have been floated to the end of the array
+    var countOfSortedValues = 0
+    
+    // Loop through unsorted portion of the array so long as a swap occurred on the prior pass
     while swapOccured {
 
-        // Starting a new pass, so reset swap flag
+        // Starting a new pass, so reset swap flag to indicate no swaps so far
         swapOccured = false
         
-        // Pass through the array to float the highest number to the end
-        for j in 0..<dataSet.count - sortedValues - 1 {
+        // Pass through unsorted portion of the the array to float the highest number to the end of the unsorted portion
+        for j in 0..<dataSet.count - countOfSortedValues - 1 {
             
             // Compare left value to right value
             if dataSet[j] > dataSet[j + 1] {
+                
                 // Swap values when left value is more than right value
                 let temporaryValue = dataSet[j] // Set aside the left value
                 dataSet[j] = dataSet[j + 1]     // Replace left with right
                 dataSet[j + 1] = temporaryValue // Replace right with temporary value
+                
+                // Track that a swap occurred
                 swapOccured = true
             }
             
         }
         
-        // Increase count of sorted values
-        sortedValues += 1
+        // Increase count of sorted values, since we just completed another pass through the array
+        countOfSortedValues += 1
 
-//        // DEBUG: Print the array after n passes
-//        print("Array after pass \(i + 1):")
+//        // DEBUG: Print the array after the last pass
+//        print("Array after pass \(countOfSortedValues):")
 //        print(dataSet)
         
     }
 
 //    // DEBUG: Print the sorted array
-//    print("array after sort is: \(dataSet)")
+//    print("Array after sort complete is: \(dataSet)")
     
 }
